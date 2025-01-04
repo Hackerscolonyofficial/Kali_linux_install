c4e9dca410f38838c0fe91a8e156b9276664dc9e
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2025-01-04 09:58:13 +0530

    Add files via upload

---
+++ install_kali.sh
@@ -0,0 +1,6 @@
+termux-setup-storage -y
+apt update ; apt upgrade -y
+pkg install wget -y
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
