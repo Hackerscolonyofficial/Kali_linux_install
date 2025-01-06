d221f0af983699138b3dcbd0d151b95d7e3427bd
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2025-01-06 17:18:06 +0530

    Delete install_kali.sh

--- install_kali.sh
+++
@@ -1,6 +0,0 @@
-termux-setup-storage -y
-apt update ; apt upgrade -y
-pkg install wget -y
-wget -O install-nethunter-termux https://offs.ec/2MceZWr
-chmod +x install-nethunter-termux
-./install-nethunter-termux
